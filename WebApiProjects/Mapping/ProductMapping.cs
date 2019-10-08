37d6c3fa3077ced7d05077aa026928ce3bb0e792
cihan Çakır <cihancakir@trakya.edu.tr>
2019-10-09 00:14:06 +0300

    AutoMapping & ProductResource eklendi

---
+++ WebApiProjects/Mapping/ProductMapping.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using WebApiProjects.Domain;
+using WebApiProjects.Resource;
+
+namespace WebApiProjects.Mapping
+{
+    public class ProductMapping :Profile
+    {
+        public ProductMapping()
+        {
+            CreateMap<ProductResource, Product>();
+
+            CreateMap<Product, ProductResource>();
+        }
+    }
+}
