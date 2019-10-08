c3be074be326199ef4ee349bd61d2d0524325dc2
cihan Çakır <cihancakir@trakya.edu.tr>
2019-10-08 16:44:19 +0300

    UnitOfWork Katmanı bitirildi

---
+++ WebApiProjects/Domain/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiProjects.Domain.UnitOfWork
+{
+    interface  IUnitOfWork
+    {
+        Task CompleteAsync();
+
+    }
+}
---
+++ WebApiProjects/Domain/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiProjects.Domain.UnitOfWork
+{
+     
+
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly WebApiContext context;
+
+        public UnitOfWork(WebApiContext context)
+        {
+            this.context = context;
+        }
+        public async Task CompleteAsync()
+        {
+            await this.context.SaveChangesAsync();
+            //Unit Of Work Pattern bu kadar Basit
+        }
+    }
+}
