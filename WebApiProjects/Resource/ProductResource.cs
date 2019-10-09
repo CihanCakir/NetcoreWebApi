ddae3fffc6f911b6446df026f0f2c968193bd403
cihan Çakır <cihancakir@trakya.edu.tr>
2019-10-09 11:58:29 +0300

    Model State Extension Tanımlamas yapıldı

--- WebApiProjects/Resource/ProductResource.cs
+++ WebApiProjects/Resource/ProductResource.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
-
+using FluentValidation;
 namespace WebApiProjects.Resource
 {
-    public class ProductResource 
+    public class ProductResource
     {
         public string Name { get; set; }
         public string Category { get; set; }
