95ad9bb0a936116da422b56e0328300df10b55f9
cihan Çakır <cihancakir@trakya.edu.tr>
2019-10-10 13:56:02 +0300

    RevokeRefreshToken Tamamlandı

--- WebApiProjects/Security/Token/TokenHandler.cs
+++ WebApiProjects/Security/Token/TokenHandler.cs
@@ -1,7 +1,11 @@
 ﻿using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Security.Cryptography;
 using System.Threading.Tasks;
 using WebApiProjects.Domain;
 
@@ -14,14 +18,55 @@ namespace WebApiProjects.Security.Token
         {
             this.tokenOptions = tokenOptions.Value;
         }
+        private string CreateRefreshToken()
+        {
+            var numberByte = new Byte[32];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(numberByte);
+                return Convert.ToBase64String(numberByte);
+            }
+        }
         public AccessToken CreateAccessToken(User user)
         {
-            throw new NotImplementedException();
-        }
+            var accessTokenExpiration = DateTime.Now.AddMinutes(tokenOptions.AccessTokenExpiration);
+            var securityKey = SignHandler.GetSecurityKey(tokenOptions.SecurityKey);
 
+            SigningCredentials signingCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256Signature);
+
+            JwtSecurityToken jwtSecurityToken = new JwtSecurityToken
+                (
+                issuer: tokenOptions.Issuer,
+                audience: tokenOptions.Audience,
+                expires: accessTokenExpiration,
+                notBefore: DateTime.Now,
+                claims:GetClaims(user),
+                signingCredentials : signingCredentials 
+                );
+            var handler = new JwtSecurityTokenHandler();
+            var token = handler.WriteToken(jwtSecurityToken);
+            AccessToken accessToken = new AccessToken();
+
+            accessToken.Token = token;
+            accessToken.RefreshToken = CreateRefreshToken();
+            accessToken.Expiration = accessTokenExpiration;
+            return accessToken;
+        }
+        private IEnumerable<Claim> GetClaims(User user)
+        {
+            var claim = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier,user.Id.ToString()),
+                new Claim(JwtRegisteredClaimNames.Email,user.Email),
+                new Claim(ClaimTypes.Name,$"{user.Name}{user.SurName}"),
+                new Claim(JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString())
+            };
+
+            return claim;
+        }
         public void RevokeRefreshToken(User user)
         {
-            throw new NotImplementedException();
+            user.RefreshToken = null;
         }
     }
 }
